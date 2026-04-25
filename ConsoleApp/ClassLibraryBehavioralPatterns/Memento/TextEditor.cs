56bd893e69c5f79deb08d37b93f65f0434e4c438
illyacherepanov <zipz241_chii@student.ztu.edu.ua>
2026-04-25 17:52:42 +0300

    Added lab4 content to git

---
+++ ConsoleApp/ClassLibraryBehavioralPatterns/Memento/TextEditor.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibraryBehavioralPatterns.Memento
+{
+    public class TextEditor
+    {
+        private string? _state;
+
+        public IMemento CreateShapshot()
+        {
+            TextDocument document = new TextDocument(_state, DateTime.Now);
+            return document;
+        }
+
+        public void Restore(IMemento memento)
+        {
+            if (memento is TextDocument document)
+            {
+                this._state = document.GetText();
+                Console.WriteLine($"Відновлено текст: {this._state}");
+            }
+        }
+
+        public void Write(string text) => _state = text;
+        public string? GetContent() => _state;
+    }
+}
